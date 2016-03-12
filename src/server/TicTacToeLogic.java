7c4a99ca75816c40882e2d2fe7bb027c0dc93f22
Emily <emily.puth@gmail.com>
2016-03-11 16:03:56 -0800

    added checkWin but it isn't complete yet

--- src/server/TicTacToeLogic.java
+++ src/server/TicTacToeLogic.java
@@ -1,5 +1,18 @@
 package server;
 
+import javax.swing.*;
+
 public class TicTacToeLogic {
+
+
+    public TicTacToeLogic() {
+    }
+
+    public void checkWin(GameState state) {
+        JLabel[] grid = state.getGrid();
+        for (int i=2; i<grid.length; i++)
+            if (grid[i-2].getIcon() != null && grid[i-1].getIcon() != null && grid[i].getIcon() != null)
+                System.out.println("winner");
+    }
 
 }
