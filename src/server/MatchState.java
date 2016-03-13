754adabded2fd5233319789e4c1dc9148eec554b
Emily <emily.puth@gmail.com>
2016-03-13 16:48:11 -0700

    score panel updates

--- src/server/MatchState.java
+++ src/server/MatchState.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 
 public class MatchState extends GameState{
 
-    private String gameName = "Tic-Tac-Toe";
+    private String gameName = "Memory Match";
     private Integer currentPlayer = 1;
     private HashMap<Integer, Integer> scoresMap;
     private JLabel grid[];
@@ -81,6 +81,12 @@ public class MatchState extends GameState{
         else if (currentPlayer == 2)
             currentPlayer = 1;
         clearClickedPanels();
+    }
+
+    public void addPoint(int playerNum) {
+        int newScore = scoresMap.get(playerNum)+1;
+        scoresMap.remove(playerNum);
+        scoresMap.put(playerNum, newScore);
     }
 
     public void addClickedPanel(JLabel clickedPanel) {
